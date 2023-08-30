b7f949c11afb3818a73f056ed134db948a2abd59
Efa Higi <efawakene2005@gmail.com>
2023-08-29 19:58:57 -0500

    Infrastracture

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
-require('dotenv').config();
-require('./config/database')
 const express = require('express');
 const path = require('path');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
+
+// Always require and configure near the top
+require('dotenv').config();
+// Connect to the database
+require('./config/database');
 
 const app = express();
 
@@ -15,18 +18,23 @@ app.use(express.json());
 app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'build')));
 
+// Middleware to check and verify a JWT and
+// assign the user object from the JWT to req.user
+app.use(require('./config/checkToken'));
+
 // Put API routes here, before the "catch all" route
 app.use('/api/users', require('./routes/api/users'));
+
 // The following "catch all" route (note the *) is necessary
 // to return the index.html on all non-AJAX requests
-app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-  });
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 // Configure to use port 3001 instead of 3000 during
 // development to avoid collision with React's dev server
 const port = process.env.PORT || 3001;
 
-app.listen(port, function() {
-  console.log(`Express app running on port ${port}`)
+app.listen(port, function () {
+  console.log(`Express app running on port ${port}`);
 });
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 // Import the top-level BrowserRouter component
 import { BrowserRouter as Router } from 'react-router-dom';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
