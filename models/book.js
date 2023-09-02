130cdc34c182432291742084daedd0c661c308ac
Efa Higi <efawakene2005@gmail.com>
2023-09-02 13:22:39 -0500

    14

--- models/book.js
+++ models/book.js
@@ -1,26 +1,24 @@
 // import mongoose from "mongoose";
 const mongoose = require('mongoose');
-const BookSchema = new mongoose.Schema({
+
+const bookSchema = new mongoose.Schema({
   title: {
     type: String,
-    required: true
+    required: true,
+    trim: true,
   },
-  // img: {
-  //   type: String,
-  //   required: true
-  // },
-  author: {
-    type: String,
-    required: true
+  price: {
+    type: Number,
+    required: true,
+    min: 0,
   },
-  description: {
-    type: String
-  },
-  updated_date: {
-    type: Date,
-    default: Date.now
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'BookCategory',
+    required: true,
   }
 });
 
-const Book = mongoose.model('Book', BookSchema);
+const Book = mongoose.model('Book', bookSchema);
+
 module.exports = Book;
