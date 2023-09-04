7395feccdb5ed3f809a4156df80e2f194c4dd00c
Efa Higi <efawakene2005@gmail.com>
2023-09-03 21:18:48 -0500

    23

--- routes/api/books.js
+++ routes/api/books.js
@@ -1,11 +1,14 @@
-const express = require('express');
+// const express = require('express');
 const router = express.Router();
 const isLoggedIn = require('../../config/ensureLoggedIn');
 const bookController = require('../../controllers/api/books');
 
+
 router.get('/', isLoggedIn, bookController.getAllBooks);
-router.post('/', isLoggedIn, bookController.createBook);
-router.put('/:id', isLoggedIn, bookController.updateBook);
+router.get('/:id',isLoggedIn, bookController.fetchBookById);
+router.post('/', isLoggedIn, bookController.upload.single('image'), bookController.createBook);
+//router.post('/', isLoggedIn, bookController.createBook);
+router.put('/:id', isLoggedIn, bookController.upload.single('image'),bookController.updateBook);
 router.delete('/:id', isLoggedIn, bookController.deleteBook);
 
 module.exports = router;
