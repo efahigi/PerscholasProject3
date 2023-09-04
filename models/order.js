301151bd9ae4fac40ca25ded346b4e7879d216c1
Efa Higi <efawakene2005@gmail.com>
2023-09-03 21:10:16 -0500

    22

--- models/order.js
+++ models/order.js
@@ -1,7 +1,30 @@
 const mongoose = require('mongoose');
 
-const bookCategorySchema = new mongoose.Schema({
-  name: String,
+const OrderSchema = new mongoose.Schema({
+ /* userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },*/
+  bookId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Book',
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ['Pending', 'Completed'],
+    default: 'Pending',
+  },
+  
+  datePlaced: {
+    type: Date
+  }
+
 });
 
-module.exports = mongoose.model('BookCategory', bookCategorySchema);
+module.exports = mongoose.model('Order', OrderSchema);
