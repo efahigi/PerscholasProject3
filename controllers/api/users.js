aa946e74e915f700fc8e349f70f30317b8ab1563
Efa Higi <efawakene2005@gmail.com>
2023-09-07 01:04:12 -0500

    Final

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -1,38 +1,98 @@
-const User = require('../../models/User');
+// const User = require('../../models/User');
+// const jwt = require('jsonwebtoken');
+// const bcrypt = require('bcrypt');
+
+// const create = async (req, res) => {
+//   try {
+//     // Add user to database
+//     const user = await User.create(req.body);
+
+//     // Helper funtion to create JWT
+//     const token = createJWT(user);
+
+//     // Responding with our jwt
+//     res.json(token);
+//   } catch (err) {
+//     // Client will check for non-2xx status code
+//     // 400 = Bad Request
+//     res.status(400).json(err);
+//   }
+// };
+
+// const login = async (req, res) => {
+//   try {
+//     // Find the user by their email
+//     const user = await User.findOne({ email: req.body.email });
+
+//     const isMatch = await bcrypt.compare(req.body.password, user.password);
+
+//     if (!isMatch) throw new Error();
+
+//     res.status(200).json(createJWT(user));
+//   } catch (err) {
+//     res.status(400).json({ msg: err.message, reason: 'Bad Credentials' });
+//   }
+// };
+
+// function createJWT(user) {
+//   return jwt.sign(
+//     // data payload
+//     { user },
+//     process.env.SECRET,
+//     { expiresIn: '24h' }
+//   );
+// }
+
+// function checkToken(req, res) {
+//   console.log('req.user', req.user);
+//   res.json(req.exp);
+// }
+
+// module.exports = {
+//   create,
+//   login,
+//   checkToken,
+// };
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
+const User = require('../../models/user');
 
-const create = async (req, res) => {
+module.exports = {
+  create,
+  login,
+  checkToken
+};
+
+function checkToken(req, res) {
+  console.log('req.user', req.user);
+  res.json(req.exp);
+}
+
+async function create(req, res) {
   try {
-    // Add user to database
+    // Add the user to the db
     const user = await User.create(req.body);
-
-    // Helper funtion to create JWT
     const token = createJWT(user);
-
-    // Responding with our jwt
     res.json(token);
   } catch (err) {
-    // Client will check for non-2xx status code
-    // 400 = Bad Request
     res.status(400).json(err);
   }
-};
+}
 
-const login = async (req, res) => {
+async function login(req, res) {
   try {
-    // Find the user by their email
-    const user = await User.findOne({ email: req.body.email });
-
-    const isMatch = await bcrypt.compare(req.body.password, user.password);
-
-    if (!isMatch) throw new Error();
-
-    res.status(200).json(createJWT(user));
+    const user = await User.findOne({email: req.body.email});
+    if (!user) throw new Error();
+    const match = await bcrypt.compare(req.body.password, user.password);
+    if (!match) throw new Error();
+    const token = createJWT(user);
+    res.json(token);
   } catch (err) {
-    res.status(400).json({ msg: err.message, reason: 'Bad Credentials' });
+    res.status(400).json('Bad Credentials');
   }
-};
+}
+
+/*--- Helper Functions --*/
 
 function createJWT(user) {
   return jwt.sign(
@@ -42,14 +102,3 @@ function createJWT(user) {
     { expiresIn: '24h' }
   );
 }
-
-function checkToken(req, res) {
-  console.log('req.user', req.user);
-  res.json(req.exp);
-}
-
-module.exports = {
-  create,
-  login,
-  checkToken,
-};
